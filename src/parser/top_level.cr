ab3d0fe1c03d63078c40ad7b432db80611c874fa
Szikszai Gusztáv <guszti5@hotmail.com>
2023-09-03 11:10:12 +0200

    Update top_level.cr

--- src/parser/top_level.cr
+++ src/parser/top_level.cr
@@ -6,22 +6,18 @@ module Mint
 
     def self.parse(contents, file) : Ast
       parser = new(contents, file)
-      parser.top_levels
+      parser.parse
       parser.eof!
-
-      if error = parser.errors.first?
-        raise error
-      end
-
+      parser.errors.first?.try { |error| raise error }
       parser.ast
     end
 
-    def top_levels : Nil
+    def parse : Nil
       items = many do
-        component ||
-          module_definition ||
+        module_definition ||
           record_definition ||
           self.enum ||
+          component ||
           provider ||
           locale ||
           routes ||
@@ -32,26 +28,26 @@ module Mint
 
       items.each do |item|
         case item
-        when Ast::Suite
-          @ast.suites << item
-        when Ast::Provider
-          @ast.providers << item
-        when Ast::RecordDefinition
-          @ast.records << item
         when Ast::Component
           @ast.components << item
-        when Ast::Module
-          @ast.modules << item
-        when Ast::Store
-          @ast.stores << item
-        when Ast::Routes
-          @ast.routes << item
-        when Ast::Enum
-          @ast.enums << item
+        when Ast::Provider
+          @ast.providers << item
         when Ast::Comment
           @ast.comments << item
+        when Ast::RecordDefinition
+          @ast.records << item
         when Ast::Locale
           @ast.locales << item
+        when Ast::Module
+          @ast.modules << item
+        when Ast::Routes
+          @ast.routes << item
+        when Ast::Store
+          @ast.stores << item
+        when Ast::Suite
+          @ast.suites << item
+        when Ast::Enum
+          @ast.enums << item
         end
       end
     end

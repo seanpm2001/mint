598985630f3be90077d2c07000c1bd6dd911febe
Szikszai Gusztáv <guszti5@hotmail.com>
2023-09-04 22:14:27 +0200

    Remove unused enum record.

--- src/type_checkers/record.cr
+++ src/type_checkers/record.cr
@@ -6,35 +6,25 @@ module Mint
           .fields
           .to_h { |field| {field.key.value, resolve(field, should_create_record)} }
 
-      case node
-      when Ast::EnumRecord
-        params =
-          fields.each_with_object({} of String => Checkable) do |(key, type), memo|
-            memo[key] = type
-          end
+      record =
+        records.find(&.==(fields))
 
-        Record.new("", params)
-      else
-        record =
-          records.find(&.==(fields))
+      record = create_record(fields) if should_create_record && !record
 
-        record = create_record(fields) if should_create_record && !record
+      error! :record_not_found_matching_record_definition do
+        block "I could not find a record that matches this structure:"
 
-        error! :record_not_found_matching_record_definition do
-          block "I could not find a record that matches this structure:"
+        snippet Record.new("", fields)
+        snippet "It was used here:", node
+      end unless record
 
-          snippet Record.new("", fields)
-          snippet "It was used here:", node
-        end unless record
+      types[node] = record
 
-        types[node] = record
-
-        node.fields.each do |field|
-          record_field_lookup[field] = record.name
-        end
-
-        record
+      node.fields.each do |field|
+        record_field_lookup[field] = record.name
       end
+
+      record
     end
   end
 end
